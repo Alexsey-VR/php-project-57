a40d6598aa06a3f9788acaffa5600c5d21c08ec0
Alexsey-VR <vityaz.iss@yandex.ru>
2026-07-22 17:42:21 +0000

    added rollbar error tracker

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use Rollbar\Laravel\RollbarServiceProvider;
 
 return [
     AppServiceProvider::class,
+    RollbarServiceProvider::class
 ];
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-rollbar', function () {
+    // Send a test log message
+    \Log::debug('Test debug message from laravel');
+
+    // Trigger a test exception
+    throw new \Exception('Test exception from Laravel');
+
+    return 'Check Rollbar for the test log and exception.';
+});
